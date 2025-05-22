3ccd059bfe852e145ae8411a522aed51e8714722
Delna George <Delnaelizabeth.George@triosstudent.com>
2025-05-22 08:21:02 -0400

    Added files

--- database_error.php
+++ database_error.php
@@ -1,32 +1,3 @@
-<?php
-  // session_start();
-?>
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="txt/css" href="css/main.css"/>
-  <title>Library Manager - Database Error</title>
-</head>
-<body>
-    <?php //include('header.php');?>
-
-    <main>
-      <h2>Database Error</h2>
-      <p>There was an error connecting to the database.</p>
-      <p>The database must be installed.</p>
-      <p>MySQL must be running.</p>
-      <p>Error Message: <?php //echo $_SESSION["database_error"]?>.</p>
-
-      <p><a href="index.php">View Books List</a></p>
-
-    </main>
-    <?php //include('footer.php')?>
-</body>
-</html> -->
-
 
 <?php
   session_start();
